8ec5671904607a7176b90cda065d2fea973bd06d
DarkLegacy22 <someshbgd3@gmail.com>
2023-10-20 02:12:25 -0700

    Exe submitted

---
+++ Application/PCApplicationScreen.cmd
@@ -0,0 +1,6 @@
+
+@echo off
+rem This script was created by Nuitka to execute 'PCApplicationScreen.exe' with Python DLL being found.
+set PATH=c:\users\somes\anaconda3;%PATH%
+set PYTHONHOME=C:\Users\somes\anaconda3
+"%~dp0PCApplicationScreen.exe" %*
